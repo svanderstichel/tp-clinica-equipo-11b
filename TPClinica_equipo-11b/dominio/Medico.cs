6c71a95f6938156ea46fc3c104af2708e009d80f
caromgan <cmgandin@live.com>
2025-10-20 17:07:27 -0300

    clase paciente y medico

---
+++ TPClinica_equipo-11b/dominio/Medico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Medico
+    {
+        public int IdMedico { get; set; }
+        [DisplayName("Código Mèdico")]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Especialidad { get; set; }
+        public string Matricula { get; set; }
+        public DateTime fechaIngreso { get; set; }
+        public DateTime FechaEgreso { get; set; }
+       public string Telefono { get; set; }
+        [DisplayName("Telèfono")]
+        public string Email { get; set; }
+    }
+}
