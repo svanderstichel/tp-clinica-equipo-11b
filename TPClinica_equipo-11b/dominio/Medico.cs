9378b3833d136d11d78bb44a01a4f010fa4adb0d
Carolina gandin <carolina.gandin@axonier.com.ar>
2025-10-21 13:30:38 -0300

    agregado de las clases necesarias para el proyecto

---
+++ TPClinica_equipo-11b/dominio/Medico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Medico
+    {
+        public int IdMedico { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Matricula { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+       
+
+
+    }
+}
