48d3b761970fb16472de00b01c8432d9cf30f312
caromgan <cmgandin@live.com>
2025-10-27 14:13:16 -0300

    clase negocio especialidad y obra social

---
+++ TPClinica_equipo-11b/negocio/EspecialidadNegocio.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class EspecialidadNegocio
+    {
+
+        public List<Especialidad> ListarEspecialidades()
+        {
+            List<Especialidad> lista = new List<Especialidad>();
+            AccesoDatos datos = new AccesoDatos();
+
+            datos.SetearConsulta("SELECT IdEspecialidad, Nombre, Descripcion FROM Especialidad");
+
+            try
+            {
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Especialidad esp = new Especialidad();
+                    esp.IdEspecialidad = (int)datos.Lector["IdEspecialidad"];
+                    esp.Nombre = Convert.ToString(datos.Lector["Nombre"]);
+                    esp.Descripcion = Convert.ToString(datos.Lector["Descripcion"]);
+
+                    lista.Add(esp);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+        public void Agregar(Especialidad nueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO Especialidad (Nombre, Descripcion) VALUES (@Nombre, @Descripcion)");
+                datos.setearParametro("@Nombre", nueva.Nombre);
+                datos.setearParametro("@Descripcion", nueva.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public void Actualizar(Especialidad especialidad)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("UPDATE Especialidad SET Nombre = @Nombre, Descripcion = @Descripcion WHERE IdEspecialidad = @IdEspecialidad");
+                datos.setearParametro("@IdEspecialidad", especialidad.IdEspecialidad);
+                datos.setearParametro("@Nombre", especialidad.Nombre);
+                datos.setearParametro("@Descripcion", especialidad.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+    }
+}
---
+++ TPClinica_equipo-11b/negocio/ObraSocialNegocio.cs
@@ -0,0 +1,90 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace negocio
+{
+    public class ObraSocialNegocio
+    {
+        public List<ObraSocial> ListarObrasSociales()
+        {
+            List<ObraSocial> lista = new List<ObraSocial>();
+            AccesoDatos datos = new AccesoDatos();
+
+            datos.SetearConsulta("SELECT IdObraSocial, Nombre, Descripcion FROM ObraSocial");
+
+            try
+            {
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    ObraSocial obra = new ObraSocial();
+                    obra.IdObraSocial = (int)datos.Lector["IdObraSocial"];
+                    obra.Nombre = Convert.ToString(datos.Lector["Nombre"]);
+                    obra.Descripcion = Convert.ToString(datos.Lector["Descripcion"]);
+
+                    lista.Add(obra);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+        public void Agregar(ObraSocial nueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO ObraSocial (Nombre, Descripcion) VALUES (@Nombre, @Descripcion)");
+                datos.setearParametro("@Nombre", nueva.Nombre);
+                datos.setearParametro("@Descripcion", nueva.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public void Actualizar(ObraSocial obra)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("UPDATE ObraSocial SET Nombre = @Nombre, Descripcion = @Descripcion WHERE IdObraSocial = @IdObraSocial");
+                datos.setearParametro("@IdObraSocial", obra.IdObraSocial);
+                datos.setearParametro("@Nombre", obra.Nombre);
+                datos.setearParametro("@Descripcion", obra.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+    }
+}
