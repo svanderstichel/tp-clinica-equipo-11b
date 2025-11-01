4b30cf26e32b95fd123c086db90182dd2e959abc
caromgan <cmgandin@live.com>
2025-10-31 22:25:45 -0300

    crear medico y conexion a base de datos

---
+++ TPClinica_equipo-11b/negocio/MedicoNegocio.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using Microsoft.SqlServer.Server;
+
+namespace negocio
+{
+    public class MedicoNegocio
+    {
+        public List<Medico> ListarMedicos(string id = "")
+        {
+            List<Medico> lista = new List<Medico>();
+            AccesoDatos datos = new AccesoDatos();
+
+            datos.SetearConsulta("SELECT IdMedico, Nombre, Apellido, Email,Telefono, IdTurnoTrabajo  FROM Medico ");
+           // datos.SetearConsulta("SELECT IdMedico, Nombre, Apellido, Email,Telefono, IdTurnoTrabajo, IdEspecialidad FROM Medico ");
+            try
+            {
+                datos.ejecutarLectura();
+
+
+
+                while (datos.Lector.Read())
+                {
+                    Medico esp = new Medico();
+                    esp.IdMedico= (int)datos.Lector["IdMedico"];
+                    esp.Nombre = Convert.ToString(datos.Lector["Nombre"]);
+                    esp.Apellido = Convert.ToString(datos.Lector["Apellido"]);
+                    esp.Email = Convert.ToString(datos.Lector["Email"]);
+                    esp.Telefono= Convert.ToString(datos.Lector["Telefono"]);
+                    esp.TurnoTrabajo.IdTurnoTrabajo = (int)datos.Lector["IdTurnoTrabajo"];
+                    //esp.IdEspecialidad = (int)datos.Lector["IdEspecialidad"];
+
+                    lista.Add(esp);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+        public void Agregar(Medico nueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO Medico (Nombre, Apellido, Matricula, Email, Telefono, IdTurnoTrabajo) VALUES (   '@Nombre',    '@Apellido',    '@Matricula',     '@Email',     '@Telefono'    ;)");
+                datos.setearParametro("@Nombre", nueva.Nombre);
+                datos.setearParametro("@Apellido", nueva.Apellido);
+                datos.setearParametro("@Matricula", nueva.Matricula);
+                datos.setearParametro("@Email", nueva.Email);
+                datos.setearParametro("@Telefono", nueva.Telefono);
+                datos.setearParametro("@IdTurnoTrabajo", nueva.TurnoTrabajo.IdTurnoTrabajo);
+
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        
+
+        public void Actualizar(Medico medico)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("UPDATE Medico SET Nombre = '@Nombre', Apellido = '@Apellido', Matricula = '@Matricula',Email = '@Email',Telefono= '@Telefono',IdTurnoTrabajo = '@IdTurnoTrabajo' WHERE IdMedico = '@IdMedico'");
+                datos.setearParametro("@IdMedico", medico.IdMedico);
+                datos.setearParametro("@Nombre", medico.Nombre);
+                datos.setearParametro("@Apellido", medico.Apellido);
+                datos.setearParametro("@Matricula", medico.Matricula);
+                datos.setearParametro("@Email", medico.Email);
+                datos.setearParametro("@Telefono", medico.Telefono);
+                datos.setearParametro("@IdTurnoTrabajo", medico.TurnoTrabajo.IdTurnoTrabajo);
+
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+    }
+
+
+}
+
---
+++ TPClinica_equipo-11b/web-clinica/CrearMedico.aspx.cs
@@ -0,0 +1,53 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace web_clinica
+{
+    public partial class CrearMedico : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CrearMedico.aspx", false);
+        }
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // alta de una nueva especialidad en la BD
+            try
+            {
+                Medico nueva = new Medico();
+                MedicoNegocio negocio = new MedicoNegocio();
+
+                nueva.Nombre = textBox1.Text;
+                nueva.Apellido = textBox2.Text;
+                nueva.Email = textBox3.Text;
+                nueva.Telefono= textBox4.Text;
+                nueva.Matricula= textBox6.Text;
+                //nueva.TurnoTrabajo.IdTurnoTrabajo = textBox5;
+
+
+
+
+                negocio.Agregar(nueva);
+                Response.Redirect("CrearMedico.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex);
+                throw;
+                //redirigir a pantalla de error
+            }
+        }
+    }
+}
